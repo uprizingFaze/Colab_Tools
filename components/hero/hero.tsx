1ccdb4c8f385c35972250b2a2976e26d4bbed28e
uprizing Faze <uprizing.faze@protonmail.com>
2024-04-16 18:52:16 -0500

    Actualizar estilos globales en globals.css

--- components/hero/hero.tsx
+++ components/hero/hero.tsx
@@ -1,192 +1,60 @@
-"use client";
 import React from "react";
-import { ContainerScroll } from "../ui/container-scroll-animation";
-import Image from "next/image";
 import { Button } from "@/components/ui/button";
 
 export function Hero() {
   return (
-    <div className="flex flex-col overflow-hidden ">
-      <ContainerScroll
-        titleComponent={
-          <>
-            <div className="flex flex-col justify-center items-center ">
-              <button className="bg-slate-300 dark:bg-black no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6  text-white inline-block">
-                <span className="absolute inset-0 overflow-hidden rounded-full">
-                  <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
-                </span>
-                <div className="relative flex space-x-2 items-center z-10 rounded-full bg-white dark:bg-black py-0.5 px-4 ring-1 ring-black/20 dark:ring-white/30 ">
-                  <span className="text-black dark:text-white">
-                    Spots Available
-                  </span>
-                  <svg
-                    className="text-black dark:text-white"
-                    fill="none"
-                    height="16"
-                    viewBox="0 0 24 24"
-                    width="16"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M10.75 8.75L14.25 12L10.75 15.25"
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="1.5"
-                    />
-                  </svg>
-                </div>
-                <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40" />
-              </button>
-              <div className=" text-center">
-                <h1>Sitio web para trabajos en equipos </h1>
-                <div className="text-3xl sm:text-4xl lg:text-5xl xl:text-6xl gradient-text">
-                  Colabora rápido y eficientemente
-                </div>
-                <p className="text-xl mt-7 text-center ">
-                  Pon en marcha tus grandes ideas con tus compañeros rapidamente
-                  <br />
-                  en esta facil y rapida plataforma
-                </p>
-                <div>
-                  <Button size="lg" className="mt-10">
-                    Vamos a colaboarar
-                  </Button>
-                </div>
-                <div className="text-xs text-slate-600 dark:text-gray-400 mt-5">
-                  excelente rendimiento en tiempo real{" "}
-                </div>
-              </div>
+    <section className="pt-44">
+      <div className="flex flex-col overflow-hidden ">
+        <div className="flex flex-col justify-center items-center ">
+          <button className="bg-slate-300 dark:bg-black no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6  text-white inline-block">
+            <span className="absolute inset-0 overflow-hidden rounded-full">
+              <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
+            </span>
+            <div className="relative flex space-x-2 items-center z-10 rounded-full bg-white dark:bg-black py-0.5 px-4 ring-1 ring-black/20 dark:ring-white/30 ">
+              <span className="text-black dark:text-white">
+                Spots Available
+              </span>
+              <svg
+                className="text-black dark:text-white"
+                fill="none"
+                height="16"
+                viewBox="0 0 24 24"
+                width="16"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M10.75 8.75L14.25 12L10.75 15.25"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="1.5"
+                />
+              </svg>
             </div>
-          </>
-        }
-      >
-        <Image
-          src={`/linear.webp`}
-          alt="hero"
-          height={720}
-          width={1400}
-          className="mx-auto rounded-2xl object-cover h-full object-left-top"
-          draggable={false}
-        />
-      </ContainerScroll>
-    </div>
+            <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40" />
+          </button>
+          <div className="text-center mt-10">
+            <h1>Sitio web para trabajos en equipos </h1>
+            <div className="text-3xl sm:text-4xl lg:text-5xl xl:text-6xl gradient-text">
+              Colabora rápido y eficientemente
+            </div>
+            <p className="text-xl mt-7 text-center ">
+              Pon en marcha tus grandes ideas con tus compañeros rapidamente
+              <br />
+              en esta facil y rapida plataforma
+            </p>
+            <div>
+              <Button size="lg" className="mt-10">
+                Vamos a colaboarar
+              </Button>
+            </div>
+            <div className="text-xs text-slate-600 dark:text-gray-400 mt-5">
+              excelente rendimiento en tiempo real{" "}
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section>
   );
 }
-
-export const users = [
-  {
-    name: "Manu Arora",
-    designation: "Founder, Algochurn",
-    image: "https://picsum.photos/id/10/300/300",
-    badge: "Mentor",
-  },
-  {
-    name: "Sarah Singh",
-    designation: "Founder, Sarah's Kitchen",
-    image: "https://picsum.photos/id/11/300/300",
-    badge: "Mentor",
-  },
-  {
-    name: "John Doe",
-    designation: "Software Engineer, Tech Corp",
-    image: "https://picsum.photos/id/12/300/300",
-    badge: "Mentor",
-  },
-  {
-    name: "Jane Smith",
-    designation: "Product Manager, Innovate Inc",
-    image: "https://picsum.photos/id/13/300/300",
-    badge: "Mentor",
-  },
-  {
-    name: "Robert Johnson",
-    designation: "Data Scientist, DataWorks",
-    image: "https://picsum.photos/id/14/300/300",
-    badge: "Mentor",
-  },
-  {
-    name: "Emily Davis",
-    designation: "UX Designer, DesignHub",
-    image: "https://picsum.photos/id/15/300/300",
-    badge: "Mentor",
-  },
-  {
-    name: "Michael Miller",
-    designation: "CTO, FutureTech",
-    image: "https://picsum.photos/id/16/300/300",
-    badge: "Mentor",
-  },
-  {
-    name: "Sarah Brown",
-    designation: "CEO, StartUp",
-    image: "https://picsum.photos/id/17/300/300",
-  },
-  {
-    name: "James Wilson",
-    designation: "DevOps Engineer, CloudNet",
-    image: "https://picsum.photos/id/18/300/300",
-    badge: "Something",
-  },
-  {
-    name: "Patricia Moore",
-    designation: "Marketing Manager, MarketGrowth",
-    image: "https://picsum.photos/id/19/300/300",
-    badge: "Mentor",
-  },
-  {
-    name: "Richard Taylor",
-    designation: "Frontend Developer, WebSolutions",
-    image: "https://picsum.photos/id/20/300/300",
-  },
-  {
-    name: "Linda Anderson",
-    designation: "Backend Developer, ServerSecure",
-    image: "https://picsum.photos/id/21/300/300",
-  },
-  {
-    name: "William Thomas",
-    designation: "Full Stack Developer, FullStack",
-    image: "https://picsum.photos/id/22/300/300",
-    badge: "Badger",
-  },
-  {
-    name: "Elizabeth Jackson",
-    designation: "Project Manager, ProManage",
-    image: "https://picsum.photos/id/23/300/300",
-    badge: "Mentor",
-  },
-  {
-    name: "David White",
-    designation: "Database Administrator, DataSafe",
-    image: "https://picsum.photos/id/24/300/300",
-    badge: "Advocate",
-  },
-  {
-    name: "Jennifer Harris",
-    designation: "Network Engineer, NetConnect",
-    image: "https://picsum.photos/id/25/300/300",
-  },
-  {
-    name: "Charles Clark",
-    designation: "Security Analyst, SecureIT",
-    image: "https://picsum.photos/id/26/300/300",
-  },
-  {
-    name: "Susan Lewis",
-    designation: "Systems Analyst, SysAnalyse",
-    image: "https://picsum.photos/id/27/300/300",
-  },
-  {
-    name: "Joseph Young",
-    designation: "Mobile Developer, AppDev",
-    image: "https://picsum.photos/id/28/300/300",
-    badge: "Mentor",
-  },
-  {
-    name: "Margaret Hall",
-    designation: "Quality Assurance, BugFree",
-    image: "https://picsum.photos/id/29/300/300",
-    badge: "Developer",
-  },
-];

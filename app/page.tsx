de25edd66f743e9e95bfe6eee6439023815fe2ba
uprizing Faze <uprizing.faze@protonmail.com>
2024-04-16 01:52:07 -0500

    Actualizar estilos globales en globals.css

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 
-import  Hero  from "@/components/hero/hero";
+import { Hero } from "@/components/hero/hero";
+import { Info } from "@/components/info/info";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <Info />
     </main>
   );
 }
